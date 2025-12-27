43da62099eb0c0461f47df4473c84fc632106a1b
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2025-12-27 19:59:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2823,3 +2823,26 @@ bot.onText(/\/deladmin(?:\s(\d+))?/, (msg, match) => {
         bot.sendMessage(chatId, `❌ User ${userId} is not an admin.`);
     }
 });
+
+bot.onText(/\/update/, async (msg) => {
+    const chatId = msg.chat.id;
+
+    const repoRaw = "https://raw.githubusercontent.com/muhammadradityaarkan593-pixel/Kanzzz69955/main/index.js";
+
+    bot.sendMessage(chatId, "⏳ Sedang mengecek update...");
+
+    try {
+        const { data } = await axios.get(repoRaw);
+
+        if (!data) return bot.sendMessage(chatId, "❌ Update gagal: File kosong!");
+
+        fs.writeFileSync("./index.js", data);
+
+        bot.sendMessage(chatId, "✅ Update berhasil!\nSilakan restart bot.");
+
+        process.exit(); // restart jika pakai PM2
+    } catch (e) {
+        console.log(e);
+        bot.sendMessage(chatId, "❌ Update gagal. Pastikan repo dan file index.js tersedia.");
+    }
+});
