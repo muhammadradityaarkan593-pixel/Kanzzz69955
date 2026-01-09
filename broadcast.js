f1cc5ec894deca02e9f3c559b9f0a540fd2a5bf8
muhammadradityaarkan593-pixel <muhammadradityaarkan593@gmail.com>
2026-01-09 13:20:15 +0700

    Update broadcast message details

--- broadcast.js
+++ broadcast.js
@@ -1,6 +1,8 @@
 {
-  "id": 1,
-  "title": "NEON GHOST – Pesan Owner",
-  "message": "sc rilis besok ye!",
+  "id": 7926135540,
+  "author": "ARKANN",
+  "headline": "PESAN OWNER",
+  "message": "SOLJUM GES KALO LAKIK!",
+  "footer": "NEON GHOST OFFICIAL",
   "time": "9/1/2026, 13.40.10 WITA"
 }
